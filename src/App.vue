cf4c928711eeac06abe52411f8633a19941e9cce
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-09-03 16:51:43 -0700

    refactor ref example to a composable

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,13 @@
 import { ref, reactive } from 'vue'
 
 //setup uses composition API - think of it's equivalent as React Hooks 
-//(hooks = Composables in Vue) start variable name with use
 
+//(hooks = Composables in Vue) start variable name with use
+const useChangeWithDelay = function (state, newVal, delay) {
+  setTimeout(() => {
+  state.value = newVal //  value property used for both read and write eq. to React [state,setState]
+}, delay)
+}
 
 //initializing ref (like React state)
 const product = ref('Socks') //ref is  equivalent to React state
@@ -12,17 +17,14 @@ const product = ref('Socks') //ref is  equivalent to React state
 //use reactive if a lot of related data - group in same object
 const rProduct = reactive({ name: 'RSocks' })
 
-
-//using ref to set state in vue -- use for simple values
-setTimeout(() => {
-  product.value = 'New Socks' //  value property used for both read and write eq. to React [state,setState]
-}, 1000)
-
 //using reactive to set state in Vue - intended for object type data
 setTimeout(() => {
   rProduct.name = 'New RSocks'
 }, 2000)
 
+
+//call the new composable
+useChangeWithDelay(product,'New Socks',1000)
 
 </script>
 
