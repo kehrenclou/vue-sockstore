409038ce7ee7188ab77a7af932496b47f76605b9
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-09-04 15:25:19 -0700

    refactor: move ProductDisplay to components

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,13 @@
 <script setup>
 import { ref } from 'vue'
-import socksGreenImage from './assets/images/socks_green.jpeg'
-import socksBlueImage from './assets/images/socks_blue.jpeg'
+import ProductDisplay from '@/components/ProductDisplay.vue'
 
-const product = ref('Socks')
-const image = ref(socksGreenImage)
-const inStock = ref(true)
-
-const details = ref(['50% cotton', '30% wool', '20% polyester'])
-const variants = ref([
-  { id: 2234, color: 'green', image: socksGreenImage },
-  { id: 2235, color: 'blue', image: socksBlueImage }
-])
-
+const premium = ref(true)
 const cart = ref(0)
-const addToCart = () => (cart.value += 1)
-const updateImage = (variantImage) => {
-  image.value = variantImage
-}
 </script>
 
 <template>
   <div class="nav-bar"></div>
   <div class="cart">Cart({{ cart }})</div>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">
-        <img v-bind:src="image" />
-        <!--v-bind shorthand is :  -->
-      </div>
-      <div class="product-info">
-        <h1>{{ product }}</h1>
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
-        <ul>
-          <li v-for="detail in details" :key="detail.id">{{ detail }}</li>
-        </ul>
-        <div v-for="variant in variants" :key="variant.id" @mouseover="updateImage(variant.image)">
-          {{ variant.color }}
-        </div>
-        <button class="button" v-on:click="addToCart">Add to Cart</button>
-      </div>
-    </div>
-  </div>
+  <ProductDisplay :premium="premium"></ProductDisplay>
 </template>
